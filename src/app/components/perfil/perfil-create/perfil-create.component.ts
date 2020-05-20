32831270e45138700c558b2fabbecf2a60fdeb90
Lucas Fernandes <5709127-LucasFcoelho@users.noreply.gitlab.com>
2020-05-20 17:16:10 -0300

    Ajustado tela de cadastro de usuario e perfil.

--- src/app/components/perfil/perfil-create/perfil-create.component.ts
+++ src/app/components/perfil/perfil-create/perfil-create.component.ts
@@ -1,47 +1,107 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { PerfilDTO } from 'src/app/models/PerfilDTO';
-import { PerfilService } from 'src/app/services/Perfil.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { PerfilDTO } from "src/app/models/PerfilDTO";
+import { PerfilService } from "src/app/services/Perfil.service";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { startWith, map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-perfil-create',
-  templateUrl: './perfil-create.component.html',
-  styleUrls: ['./perfil-create.component.css']
+  selector: "app-perfil-create",
+  templateUrl: "./perfil-create.component.html",
+  styleUrls: ["./perfil-create.component.css"],
 })
 export class PerfilCreateComponent implements OnInit {
-
   formCreate: FormGroup;
   perfil: PerfilDTO;
+  perfils: PerfilDTO[];
   private formSubmitAttempt: boolean;
+  UpdateOrDelete: boolean = false;
+  filteredOptions: Observable<PerfilDTO[]>;
+
+  private _filter(value: string): PerfilDTO[] {
+    if (!value) return;
+
+    const filterValue = value.toLowerCase();
+    return this.perfils.filter(
+      (perfil) => perfil.funcao_perfil.toLowerCase().indexOf(filterValue) !== -1
+    );
+  }
+
   constructor(
     private perfilService: PerfilService,
     private fb: FormBuilder,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.criaFormCreate();
+    this.getAllPerfils();
+  }
+
+  autoCompletePerfil(): void {
+    this.filteredOptions = this.formCreate
+      .get("funcao_perfil")!
+      .valueChanges.pipe(
+        startWith(""),
+        map((value) => this._filter(value))
+      );
   }
 
   criaFormCreate() {
     this.formCreate = this.fb.group({
       id: [null],
-      funcao_perfil: ["", Validators.compose([Validators.required, Validators.maxLength(50)])]
+      funcao_perfil: [
+        "",
+        Validators.compose([Validators.required, Validators.maxLength(50)]),
+      ],
+    });
+  }
+
+  getAllPerfils() {
+    this.perfilService.getAllPerfil().subscribe((perfils: PerfilDTO[]) => {
+      this.perfils = perfils;
+      this.autoCompletePerfil();
+      console.log(this.perfils);
     });
   }
 
   createPerfil(): void {
     this.perfil = this.formCreate.value;
     console.log(this.perfil);
-    this.perfilService.postPerfil(this.perfil).subscribe(
-      () => {
-        this.perfilService.showMessage("Perfil criado com sucesso!");
-        this.router.navigate(["/perfils"]);
-      });
+    this.perfilService.postPerfil(this.perfil).subscribe(() => {
+      this.perfilService.showMessage("Perfil criado com sucesso!");
+    });
   }
 
-  cancelar() : void {
-    this.router.navigate(["/perfils"]);
+  updatePerfil(): void {
+    this.perfil = this.formCreate.value;
+    console.log(this.perfil);
+    this.perfilService.putPerfil(this.perfil).subscribe(() => {
+      this.perfilService.showMessage("Perfil editado com sucesso!");
+      this.router.navigate(["/perfils"]);
+    });
+  }
+
+  deletePerfil(): void {
+    this.perfil = this.formCreate.value;
+    console.log(this.perfil);
+    this.perfilService.deletePerfil(this.perfil).subscribe(() => {
+      this.perfilService.showMessage("Perfil removido com sucesso!");
+    });
+  }
+
+  updateForm(modeloPerfil: PerfilDTO): void {
+    this.UpdateOrDelete = true;
+    console.log(modeloPerfil);
+    this.formCreate.patchValue(modeloPerfil);
+  }
+
+  limparForm(): void {
+    this.getAllPerfils();
+    this.formCreate.reset();
+    this.perfil = null;
+    this.UpdateOrDelete = false;
   }
 
   isFieldInvalid(field: string) {
@@ -52,8 +112,7 @@ export class PerfilCreateComponent implements OnInit {
     );
   }
 
-  public hasError = (field: string, errorName: string) =>{
+  public hasError = (field: string, errorName: string) => {
     return this.formCreate.controls[field].hasError(errorName);
-  }
-
+  };
 }
--- src/app/components/usuario/usuario-create/usuario-create.component.ts
+++ src/app/components/usuario/usuario-create/usuario-create.component.ts
@@ -3,6 +3,8 @@ import { Usuario } from 'src/app/models/Usuario';
 import { UsuarioService } from 'src/app/services/Usuario.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-usuario-create',
@@ -13,7 +15,20 @@ export class UsuarioCreateComponent implements OnInit {
 
   formCreate: FormGroup;
   usuario: Usuario;
+  usuarios: Usuario[];
   private formSubmitAttempt: boolean;
+  UpdateOrDelete: boolean = false;
+  filteredOptions: Observable<Usuario[]>;
+  lblSenha : string = "Senha";
+  
+  private _filter(value: string): Usuario[] {
+    if(!value)
+      return;
+
+    const filterValue = value.toLowerCase();
+    return this.usuarios.filter(func => func.login.toLowerCase().indexOf(filterValue) !== -1);
+  }
+
   constructor(
     private usuarioService: UsuarioService,
     private fb: FormBuilder,
@@ -21,6 +36,14 @@ export class UsuarioCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.criaFormCreate();
+    this.getAllUsuarios();
+  }
+
+  autoCompleteUsuario(): void {
+    this.filteredOptions = this.formCreate.get('login')!.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
 
   criaFormCreate() {
@@ -34,18 +57,60 @@ export class UsuarioCreateComponent implements OnInit {
     });
   }
 
+  getAllUsuarios() 
+  {
+    this.usuarioService.getAllUsuario().subscribe(
+      (usuarios: Usuario[]) => {
+        this.usuarios = usuarios;
+        this.autoCompleteUsuario();
+        console.log(this.usuarios);
+      }
+    );
+  }
+
   createUsuario(): void {
     this.usuario = this.formCreate.value;
-    console.log(this.usuario);
+    console.log('USUARIO',this.usuario);
     this.usuarioService.postUsuario(this.usuario).subscribe(
       () => {
         this.usuarioService.showMessage("Usuário criado com sucesso!");
-        this.router.navigate(["/usuarios"]);
+        this.limparForm();
       });
   }
 
-  cancelar() : void {
-    this.router.navigate(["/usuarios"]);
+  deleteUsuario(): void {
+    this.usuario = this.formCreate.value;
+    console.log(this.usuario);
+    this.usuarioService.deleteUsuario(this.usuario).subscribe(
+      () => {
+        this.usuarioService.showMessage("Usuário removido com sucesso!");
+        this.limparForm();
+      });
+  }
+
+  updateUsuario(): void {
+    this.usuario = this.formCreate.value;
+    console.log(this.usuario);
+    this.usuarioService.putUsuario(this.usuario).subscribe(
+      () => {
+        this.usuarioService.showMessage("Usuário alterado com sucesso!");
+        this.limparForm();
+      });
+  }
+
+  updateForm(modeloUsuario: Usuario): void {
+    this.lblSenha = "Senha Atual"; 
+    this.UpdateOrDelete = true;
+    console.log(modeloUsuario);
+    this.formCreate.patchValue(modeloUsuario);
+  }
+
+  limparForm(): void {
+    this.getAllUsuarios();
+    this.formCreate.reset();
+    this.usuario = null;
+    this.UpdateOrDelete = false;
+    this.lblSenha = "Senha"; 
   }
 
   confirmSenhas(controlName: string, matchingControlName: string) {
